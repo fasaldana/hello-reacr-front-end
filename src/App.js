e1d46780165b4877e80ea806cb5c60c6dc729ab9
fasaldana <fernando_s_98@hotmail.com>
2022-08-10 01:38:17 -0500

    Add front-end UI

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
