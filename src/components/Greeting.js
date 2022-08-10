3cf9758a9a3077551c6fc39d65188538e7a61a59
fasaldana <fernando_s_98@hotmail.com>
2022-08-10 01:28:52 -0500

    first commit

--- src/components/Greeting.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-const Greeting = () => {
-  const greeting = useSelector((state) => state.greeting.value);
-  return <div>{greeting}</div>;
-}
-
-export default Greeting;
